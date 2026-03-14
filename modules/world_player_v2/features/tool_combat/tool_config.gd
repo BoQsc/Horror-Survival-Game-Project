ed2270d8388974f569c51d6d1a5edb459d9027a6
Feldwor <BoQsc@users.noreply.github.com>
2026-03-14 21:53:27 +0200

    fp_clipping_prevention_enabled off by default

--- modules/world_player_v2/features/tool_combat/tool_config.gd
+++ modules/world_player_v2/features/tool_combat/tool_config.gd
@@ -52,7 +52,7 @@ var gaze_debug_enabled: bool = true
 
 var fp_sway_enabled: bool = true
 var fp_bob_enabled: bool = true
-var fp_clipping_prevention_enabled: bool = true
+var fp_clipping_prevention_enabled: bool = false
 var fp_clipping_reach_mult: float = 1.0
 var fp_clipping_flare_mult: float = 1.0
 
